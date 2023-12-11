f2ca66da5a092add9532d98dd5cf0bf8ca6065fc
J2Y98 <qjadj98@gmail.com>
2023-12-11 19:31:32 +0900

    sql 테스트

--- src/test/java/com/itwillbs/controller/testConnection.java
+++ src/test/java/com/itwillbs/controller/testConnection.java
@@ -14,6 +14,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/root-context.xml"})
 public class testConnection {
 	
+	private final String NAMESPACE = "com.itwillbs.mapper.MemberMapper";
 	@Inject
 	private SqlSession sqlSession;
 	
@@ -22,5 +23,6 @@ public class testConnection {
 	@Test
 	public void 연결테스트() {
 		logger.debug("SqlSession : " + sqlSession);
+		logger.debug(sqlSession.selectOne(NAMESPACE + ".getTime"));
 	}
 }
